d7bee7b2d7b52723c66bfdff0f4a0dbc94fa8026
gregorypanta <grigorispantazopoulos05@gmail.com>
2026-02-12 12:31:40 +0200

    Update API URL to production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,8 @@
-const BASE_URL = "http://127.0.0.1:8000/api";
+// Από αυτό:
+// const BASE_URL = "http://127.0.0.1:8000/api";
+
+// Σε αυτό:
+const BASE_URL = "https://mental-models-backend.onrender.com/api";
 
 export const fetchData = async (endpoint) => {
   const response = await fetch(`${BASE_URL}${endpoint}`);
